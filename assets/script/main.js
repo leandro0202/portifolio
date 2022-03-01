dab8a5d0343b2388eea5463504e5d43692856e71
leandro alves de sousa <71714626+leandro0202@users.noreply.github.com>
2022-03-01 14:45:06 -0300

    alterações 0.3

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,18 +3,38 @@ const frase2 = document.querySelector('.frase2')
 
 
 
-function typeWriter(elemento, temp){
+function typeWriter(elemento, temp) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textoArray.forEach((letra, i) => {
-        setTimeout(()=> elemento.innerHTML += letra, temp * i)
+        setTimeout(() => elemento.innerHTML += letra, temp * i)
     });
 
 }
 
+typeWriter(frase, 75)
+typeWriter(frase2, 300)
+
+function hoverImage() {
+    const img = document.querySelectorAll('.header-links a img').forEach((elemento, indice) => {
+        if(indice == 0){
+            elemento.addEventListener("mouseenter", () => {
+                elemento.src = 'assets/img/github2.png'
+            })
+            elemento.addEventListener("mouseout", () => {
+                elemento.src = 'assets/img/github.png'
+            })
+        }else{
+            elemento.addEventListener("mouseenter", () => {
+                elemento.src = 'assets/img/linkdin2.png'
+            })
+            elemento.addEventListener("mouseout", () => {
+                elemento.src = 'assets/img/linkdin.png'
+            })
+        }
+  
+    })
+}
 
 
- typeWriter(frase, 75)
- typeWriter(frase2, 300)
-
-
+hoverImage()
